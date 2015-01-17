eb4622c2e8b96d66b9239c7318e2ae18c5bbd569
Yadu Nand Babuji <yadudoc1729@gmail.com>
2015-01-16 18:20:35 -0600

    Updates to docs

--- doc/build_docs.sh
+++ doc/build_docs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
 asciidoc -a icons -a toc -a toplevels=2 -a stylesheet=$PWD/asciidoc.css -a max-width=800px -o tutorial.html README
+asciidoc -a icons -a stylesheet=$PWD/asciidoc.css -a max-width=800px -o amazon_ec2.html amazon_ec2.asc
 
