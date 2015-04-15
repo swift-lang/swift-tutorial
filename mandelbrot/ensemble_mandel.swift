f98039d208dd4d496da7d7c2cb7c5908081e6f68
Yadu Nand Babuji <yadunandb@ci.uchicago.edu>
2015-04-15 14:00:34 -0500

    Updates with support for a montage image

--- mandelbrot/ensemble_mandel.swift
+++ mandelbrot/ensemble_mandel.swift
@@ -5,13 +5,13 @@ app (file image, file out, file err) mandelbrot (file mandel_sh, int iterations,
     bash @mandel_sh "-i" iterations "-s 1 -r" resolution "-f" @image stdout=@out stderr=@err;
 }
 
-app (file image, file out, file err) assemble (file[] mandel_imgs)
+app (file movie, file montage, file out, file err) assemble (file[] mandel_imgs)
 {
-    assemble @image @mandel_imgs stdout=@out stderr=@err;
+    assemble @movie @montage @mandel_imgs stdout=@out stderr=@err;
 }
 
 int  itermax     = toInt(arg("niter", "100"));     # number of iterations for mandelbrot
-int  step        = toInt(arg("step", "5"));       # number of iterations for mandelbrot
+int  step        = toInt(arg("step", "1"));       # number of iterations for mandelbrot
 int  resolution  = toInt(arg("res",  "10000"));   # Resolution of result
 
 // 5 -> 100 iterations stepping by 5
@@ -25,7 +25,8 @@ foreach i in [5:itermax:step]{
     (mandel_img[i], mandel_out[i], mandel_err[i]) = mandelbrot(mandel_sh, i, resolution);
 }
 
-file movie <"output/mandel.gif">;
+file movie   <"output/mandel.gif">;
+file montage <"output/montage.jpg">;
 file assemble_out <"output/assemble.out">;
 file assemble_err <"output/assemble.err">;
-(movie, assemble_out, assemble_err) = assemble (mandel_img);
+(movie, montage, assemble_out, assemble_err) = assemble (mandel_img);
