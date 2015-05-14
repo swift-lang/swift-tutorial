14effa09ed81179cf8b24b813271372973f2f02e
Yadu Babuji <yadunand@tg-login1.blacklight.psc.teragrid.org>
2015-05-14 14:35:47 -0400

    Updates from blacklight

--- mandelbrot/setup.sh
+++ mandelbrot/setup.sh
@@ -2,11 +2,17 @@
 
 if [[ "$HOSTNAME" == *stampede* ]]
 then
-    echo "STAMPEDE"
+    echo "XSEDE/Stampede"
     module load mvapich2
     cd bin/
     make clean; make
     cd ..
+
+elif [[ "$HOSTNAME" == *blacklight* ]]
+then
+    echo "XSEDE/Blacklight"
+    module load ImageMagick/6.7.3-10
+
 else
     SWIFT=$PWD/swift-trunk/bin
     export PATH=$SWIFT:$PWD/bin:$PATH
